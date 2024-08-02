36afdc7d6a36d86e8ca5b5369db664f51d5a2af9
blubincod <bluebinarycode@gmail.com>
2024-08-02 19:59:37 +0900

    Initial commit

---
+++ src/main/java/com/concord/prodauction/ProdAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.concord.prodauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/concord/prodauction/ProdAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concord.prodauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
